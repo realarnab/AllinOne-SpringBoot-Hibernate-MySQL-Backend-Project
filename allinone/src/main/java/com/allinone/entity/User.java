cf0c8c648302c6b9a6f278596baff6b7c18ce910
Arnab <arnabmondal604@gmail.com>
2024-06-02 19:00:06 +0530

    user entity created

---
+++ allinone/src/main/java/com/allinone/entity/User.java
@@ -0,0 +1,30 @@
+package com.allinone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String username;
+    private String password;
+    private String type;
+
+    @Column(name = "user_role",nullable = false)
+    private String userRole;
+}
