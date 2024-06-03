ab16c27bbc9afa571ee7a70b7905bbcb6e6e5d37
Arnab <arnabmondal604@gmail.com>
2024-06-04 00:19:20 +0530

    create user service logic added

--- allinone/src/main/java/com/allinone/service/impl/UserServiceimpl.java
+++ allinone/src/main/java/com/allinone/service/impl/UserServiceimpl.java
@@ -1,5 +1,6 @@
 package com.allinone.service.impl;
 
+import com.allinone.entity.User;
 import com.allinone.payload.UserDto;
 import com.allinone.repository.UserRepository;
 import com.allinone.service.UserService;
@@ -10,6 +11,21 @@ public class UserServiceimpl implements UserService {
     private UserRepository userRepository;
     @Override
     public UserDto createUser(UserDto userDto) {
-        return null;
+        User user=new User();
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setUsername(userDto.getUsername());
+        user.setPassword(userDto.getPassword());
+        user.setUserRole(userDto.getUserRole());
+
+        User saved = userRepository.save(user);
+
+        userDto.setId(saved.getId());
+        userDto.setName(saved.getName());
+        userDto.setUsername(saved.getUsername());
+        userDto.setEmail(saved.getEmail());
+        userDto.setPassword(saved.getPassword());
+        userDto.setUserRole(saved.getUserRole());
+        return userDto;
     }
 }
