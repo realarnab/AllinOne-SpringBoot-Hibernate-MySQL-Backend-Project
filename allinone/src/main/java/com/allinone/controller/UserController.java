a7f72401f9e1046dab798769d91f142180ca85fc
Arnab <arnabmondal604@gmail.com>
2024-06-06 20:17:24 +0530

    add user api implemented

--- allinone/src/main/java/com/allinone/controller/UserController.java
+++ allinone/src/main/java/com/allinone/controller/UserController.java
@@ -1,6 +1,12 @@
 package com.allinone.controller;
 
+import com.allinone.payload.UserDto;
+import com.allinone.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+    @PostMapping("/create")
+    public ResponseEntity<?> createNewUser(@RequestBody UserDto userDto){
+
+        UserDto user = userService.createUser(userDto);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
 }
