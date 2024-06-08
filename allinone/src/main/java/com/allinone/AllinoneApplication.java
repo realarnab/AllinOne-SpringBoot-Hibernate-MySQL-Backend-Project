77606ea9c9b1b14c86a9292116a9b2602fe5b562
Arnab <arnabmondal604@gmail.com>
2024-06-08 22:47:44 +0530

    modelMapper implemented

--- allinone/src/main/java/com/allinone/AllinoneApplication.java
+++ allinone/src/main/java/com/allinone/AllinoneApplication.java
@@ -1,7 +1,9 @@
 package com.allinone;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AllinoneApplication {
@@ -10,4 +12,8 @@ public class AllinoneApplication {
 		SpringApplication.run(AllinoneApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper getModelMapper(){
+		return new ModelMapper();
+	}
 }
--- allinone/src/main/java/com/allinone/service/impl/UserServiceImpl.java
+++ allinone/src/main/java/com/allinone/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.allinone.entity.User;
 import com.allinone.payload.UserDto;
 import com.allinone.repository.UserRepository;
 import com.allinone.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,23 +12,35 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ModelMapper modelMapper;
     @Override
     public UserDto createUser(UserDto userDto) {
-        User user=new User();
-        user.setName(userDto.getName());
-        user.setEmail(userDto.getEmail());
-        user.setUsername(userDto.getUsername());
-        user.setPassword(userDto.getPassword());
-        user.setUserRole(userDto.getUserRole());
+//        User user=new User();
+//        user.setName(userDto.getName());
+//        user.setEmail(userDto.getEmail());
+//        user.setUsername(userDto.getUsername());
+//        user.setPassword(userDto.getPassword());
+//        user.setUserRole(userDto.getUserRole());
+        User user = mapToEntity(userDto);
 
         User saved = userRepository.save(user);
 
-        userDto.setId(saved.getId());
-        userDto.setName(saved.getName());
-        userDto.setUsername(saved.getUsername());
-        userDto.setEmail(saved.getEmail());
-        userDto.setPassword(saved.getPassword());
-        userDto.setUserRole(saved.getUserRole());
-        return userDto;
+//        userDto.setId(saved.getId());
+//        userDto.setName(saved.getName());
+//        userDto.setUsername(saved.getUsername());
+//        userDto.setEmail(saved.getEmail());
+//        userDto.setPassword(saved.getPassword());
+//        userDto.setUserRole(saved.getUserRole());
+        UserDto dto = mapToDto(saved);
+        return dto;
+    }
+
+    public UserDto mapToDto(User user){
+        return modelMapper.map(user, UserDto.class);
+    }
+
+    public User mapToEntity(UserDto dto){
+        return modelMapper.map(dto,User.class);
     }
 }
