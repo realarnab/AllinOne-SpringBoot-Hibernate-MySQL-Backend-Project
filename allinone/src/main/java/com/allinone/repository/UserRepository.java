337e61ddd399a88bc91061b0d4c776bf1fcc8d5f
Arnab <arnabmondal604@gmail.com>
2024-06-23 19:21:10 +0530

    custom method for upadtaion implemented

--- allinone/src/main/java/com/allinone/repository/UserRepository.java
+++ allinone/src/main/java/com/allinone/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.allinone.repository;
 
 import com.allinone.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    @Modifying
+    @Query("UPDATE User u SET u.name= :name, u.email= :email WHERE u.id= :id")
+    int update(@Param("id") long id,@Param("name") String name,@Param("email") String email);
 }
