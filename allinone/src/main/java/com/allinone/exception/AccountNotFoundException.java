9d28d12b5ae2c2a80aea62770a6c77c931b54962
Arnab <arnabmondal604@gmail.com>
2024-07-03 21:08:58 +0530

    accountNotFound exception created

--- allinone/src/main/java/com/allinone/exception/AccountNotFoundException.java
+++ allinone/src/main/java/com/allinone/exception/AccountNotFoundException.java
@@ -1,2 +1,7 @@
-package com.allinone.exception;public class AccountNotFoundException {
+package com.allinone.exception;
+
+public class AccountNotFoundException extends Throwable{
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
 }
