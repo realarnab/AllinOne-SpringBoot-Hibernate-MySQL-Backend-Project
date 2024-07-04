3a431ef0dafad2b0ae819c76c8f0a68257abd776
Arnab <arnabmondal604@gmail.com>
2024-07-04 20:16:30 +0530

    securityConfig class implemented

--- allinone/src/main/java/com/allinone/config/SecurityConfig.java
+++ allinone/src/main/java/com/allinone/config/SecurityConfig.java
@@ -1,2 +1,18 @@
-package com.allinone.config;public class SecurityConfig {
+package com.allinone.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable();
+        http.authorizeHttpRequests()
+                .requestMatchers("/api/v1/users/**","/api/v1/accounts/**").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+
+    }
 }
