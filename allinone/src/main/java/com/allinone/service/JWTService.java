94b185057273c0934fb8459e6a0f5dad5c99bd89
Arnab <arnabmondal604@gmail.com>
2024-07-08 19:43:18 +0530

    jWTservice class implemented

--- allinone/src/main/java/com/allinone/service/JWTService.java
+++ allinone/src/main/java/com/allinone/service/JWTService.java
@@ -1,2 +1,39 @@
-package com.allinone.service;public class JWTService {
+package com.allinone.service;
+
+import com.allinone.entity.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JWTService { //JWT service class for token generation
+    @Value("${jwt.algo.key}") //@Value--> used for fetch the value of the variable from properties file
+    private String algoKey;
+
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    @Value("${jwt.exp.time}")
+    private int expTime;
+
+    private Algorithm algorithm;
+
+    private final static String USER_NAME = "username";
+
+    @PostConstruct
+    public void postConstruct(){
+        algorithm=Algorithm.HMAC256(algoKey);
+    }
+
+    public String generateToken(User user){
+        return JWT.create()
+                .withClaim(USER_NAME,user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+expTime))
+                .withIssuer(issuer)
+                .sign(algorithm);
+    }
 }
