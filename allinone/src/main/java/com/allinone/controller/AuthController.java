41914e92705534a2f583fc2069c45e401088c624
Arnab <arnabmondal604@gmail.com>
2024-07-10 21:47:10 +0530

    auth controller implemented

--- allinone/src/main/java/com/allinone/controller/AuthController.java
+++ allinone/src/main/java/com/allinone/controller/AuthController.java
@@ -1,2 +1,11 @@
-package com.allinone.controller;public class AuthController {
+package com.allinone.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    
 }
