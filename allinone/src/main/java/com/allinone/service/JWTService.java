cc53b0bb0d1d24abe9351dcfe27ef889361f0ae9
Arnab <arnabmondal604@gmail.com>
2024-07-10 21:45:10 +0530

    getUsername method implemented

--- allinone/src/main/java/com/allinone/service/JWTService.java
+++ allinone/src/main/java/com/allinone/service/JWTService.java
@@ -3,6 +3,7 @@ package com.allinone.service;
 import com.allinone.entity.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -35,5 +36,11 @@ public class JWTService { //JWT service class for token generation
                 .withExpiresAt(new Date(System.currentTimeMillis()+expTime))
                 .withIssuer(issuer)
                 .sign(algorithm);
+    }
+
+    public String getUsername(String token){
+        DecodedJWT decodedToken = JWT.require(algorithm)
+                .withIssuer(issuer).build().verify(token);
+        return decodedToken.getClaim(USER_NAME).asString();
     }
 }
