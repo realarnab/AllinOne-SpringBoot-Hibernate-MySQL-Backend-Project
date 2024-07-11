de5a3a83a092bdcb6006cf41ae82bc63eee4d5c1
Arnab <arnabmondal604@gmail.com>
2024-07-11 21:39:23 +0530

    login dto added

--- allinone/src/main/java/com/allinone/payload/LoginDto.java
+++ allinone/src/main/java/com/allinone/payload/LoginDto.java
@@ -1,2 +1,15 @@
-package com.allinone.payload;public class LoginDto {
+package com.allinone.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDto {
+    private String username;
+    private String password;
 }
