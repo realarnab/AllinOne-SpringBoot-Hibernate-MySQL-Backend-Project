e7ebf68e5d12a66a1f36943a875329809da73b65
Arnab <arnabmondal604@gmail.com>
2024-07-11 21:34:00 +0530

    login api created

--- allinone/src/main/java/com/allinone/controller/AuthController.java
+++ allinone/src/main/java/com/allinone/controller/AuthController.java
@@ -1,11 +1,27 @@
 package com.allinone.controller;
 
+import com.allinone.payload.LoginDto;
+import com.allinone.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+    @Autowired
+    private UserService userService;
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginDto loginDto){
+        String token = userService.verifyLogin(loginDto);
 
-    
+        if (token!=null){
+            return new ResponseEntity<>(token, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Failed",HttpStatus.UNAUTHORIZED);
+    }
 }
