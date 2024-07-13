98e732f178b3702b675c69ef2c746aa7337fb6cc
Arnab <arnabmondal604@gmail.com>
2024-07-13 16:08:57 +0530

    jwt authentication added

--- allinone/src/main/java/com/allinone/config/SecurityConfig.java
+++ allinone/src/main/java/com/allinone/config/SecurityConfig.java
@@ -1,16 +1,23 @@
 package com.allinone.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private JWTRequestFilter jwtRequestFilter;
+
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().cors().disable();
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
         http.authorizeHttpRequests()
                 .requestMatchers("/api/v1/users/**","/api/v1/accounts/**").permitAll()
+                .requestMatchers("/api/v1/auth/login").hasAnyRole("ADMIN","USER")
                 .anyRequest().authenticated();
         return http.build();
 
